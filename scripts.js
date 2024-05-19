23eca4cb928bcefa047cfd92686b4c461208c659
Diego <da.rivero@gmail.com>
2024-05-18 22:44:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,3 @@
-var currentSound = null;
-
-function playSound(soundFile, buttonElement) {
-    if (currentSound) {
-        currentSound.pause();
-        currentSound.currentTime = 0;
-        if (currentSound.buttonElement) {
-            currentSound.buttonElement.classList.remove('active');
-        }
-    }
-
-    currentSound = new Audio(soundFile);
-    currentSound.play();
-    if (buttonElement) {
-        buttonElement.classList.add('active');
-        currentSound.buttonElement = buttonElement;
-    }
-
-    currentSound.addEventListener('ended', function() {
-        if (currentSound.buttonElement) {
-            currentSound.buttonElement.classList.remove('active');
-        }
-    });
-}
-
-function stopSound() {
-    if (currentSound) {
-        currentSound.pause();
-        currentSound.currentTime = 0;
-        if (currentSound.buttonElement) {
-            currentSound.buttonElement.classList.remove('active');
-        }
-    }
-}
-
 function toggleMenu() {
     var menu = document.getElementById('menu');
     if (menu.style.display === 'block') {
@@ -42,4 +7,19 @@ function toggleMenu() {
     }
 }
 
+function playSound(soundFile, button) {
+    var audio = new Audio(soundFile);
+    audio.play();
+    button.classList.add('active');
+    audio.onended = function() {
+        button.classList.remove('active');
+    };
+}
+
+function stopSound() {
+    var sounds = document.getElementsByTagName('audio');
+    for (var i = 0; i < sounds.length; i++) {
+        sounds[i].pause();
+        sounds[i].currentTime = 0;
+    }
 }
